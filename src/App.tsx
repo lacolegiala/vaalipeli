93c4d126dc690a41a79844a7a5f61d69a0ef6f0a
Sofia Riekkola <sofia.riekkola@gmail.com>
2025-03-21 17:10:56 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+import React from 'react'
+
+function App() {
+  const [score, setScore] = useState(0)
+
+  async function getCandidateData() {
+  }
+
+  getCandidateData()
+
+  return (
+    <>
+    </>
+  )
+}
+
+export default App
