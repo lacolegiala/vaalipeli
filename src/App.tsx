cbccdb8a73ae5e273af2bdcad7c788df1e1af1f4
Sofia Riekkola <sofia.riekkola@gmail.com>
2025-03-24 15:16:24 +0200

    Get municipality and county data

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,37 @@
 import { useState } from 'react'
 import './App.css'
 import React from 'react'
+import axios from 'axios'
 
 function App() {
   const [score, setScore] = useState(0)
+  const [candidates, setCandidates] = useState<Candidate[]>([])
+  const [counties, setCounties] = useState<County[]>([])
+  const [municipalities, setMunicipalities] = useState<Municipality[]>([])
 
   async function getCandidateData() {
+    try {
+      const { data: countyData } = await axios.get<County[]>('http://localhost:5002/counties')
+      setCounties(countyData)
+      const { data: municipalityData } = await axios.get<Municipality[]>('http://localhost:5002/municipalities')
+      setMunicipalities(municipalityData)
+    } catch (error) {
+      console.error("Error fetching game data", error);
+    }
   }
 
   getCandidateData()
 
   return (
-    <>
-    </>
+    <div>
+      {municipalities.map((municipality) => {
+        return (
+          <li>
+            {municipality.name_fi}
+          </li>
+        )
+      })}
+    </div>
   )
 }
 
