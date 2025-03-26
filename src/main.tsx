b79bb15ac660e34cdf3821d6e8a2b918b640cede
Sofia Riekkola <sofia.riekkola@gmail.com>
2025-03-26 20:21:11 +0200

    Fix ts bug

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.js'
 import React from 'react'
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
