c9ab6689a17aaae5caa73ea68fb524be3d0e7cf5
Sofia Riekkola <sofia.riekkola@gmail.com>
2025-03-28 19:47:21 +0200

    Add score count

--- src/GameView.tsx
+++ src/GameView.tsx
@@ -8,27 +8,36 @@ type GameViewProps = {
 
 const GameView: React.FC<GameViewProps> = (props: GameViewProps) => {
   const [round, setRound] = useState(1)
+  const [score, setScore] = useState(0)
 
   useEffect(() => console.log(round, props.gameData), [round])
+
+  const handlePickChoice = (id: number) => {
+    if (id === props.gameData.rounds[round - 1].correctCandidateId) {
+      setScore(score + 1)
+    }
+    setRound(round + 1)
+  }
 
   return (
     <div>
       {round < 11 && 
         <div>
-          {round}
+          {round} / 10
+          <div>Pisteet: {score}</div>
           <div>
             {props.gameData.rounds[round - 1].promise}
           </div>
           <div>
-            <button onClick={() => setRound(round + 1)}>{props.gameData.rounds[round - 1].candidates[0].first_name} {props.gameData.rounds[round - 1].candidates[0].last_name}</button>
+            <button onClick={() => handlePickChoice(props.gameData.rounds[round - 1].candidates[0].id)}>{props.gameData.rounds[round - 1].candidates[0].first_name} {props.gameData.rounds[round - 1].candidates[0].last_name}</button>
           </div>
           <div>
-            <button onClick={() => setRound(round + 1)}>{props.gameData.rounds[round - 1].candidates[1].first_name} {props.gameData.rounds[round - 1].candidates[1].last_name}</button>
+            <button onClick={() => handlePickChoice(props.gameData.rounds[round - 1].candidates[1].id)}>{props.gameData.rounds[round - 1].candidates[1].first_name} {props.gameData.rounds[round - 1].candidates[1].last_name}</button>
           </div>
         </div>
       }
       {round > 10 &&
-        <div>Game over</div>
+        <div>Game over! Score: {score}</div>
       }
     </div>
   )
