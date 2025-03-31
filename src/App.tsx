4a37f25001c8b5bb1ab45c64cc098469762b9f77
Sofia Riekkola <sofia.riekkola@gmail.com>
2025-03-31 12:07:40 +0300

    Add back to main menu button

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,22 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import './App.css'
 import React from 'react'
-import { Route, Router, Routes } from 'react-router-dom';
-import { createBrowserHistory } from "history";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import GameView from './components/GameView';
 import { GameData } from './types';
-
-const history = createBrowserHistory();
 
 function App() {
   const [gameData, setGameData] = useState<GameData>();
 
   return (
-    <div>
-     <Router 
-        navigator={history}
-        location={history.location}
-      >
-        <Routes>
-          <Route path='/' element={<Home setGameData={setGameData} />} />
-          <Route path='/play' element={gameData ? <GameView gameData={gameData} /> : <Home setGameData={setGameData}/>} />
-        </Routes>
-     </Router>
-    </div>
-  )
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home setGameData={setGameData} />} />
+        <Route path='/play' element={gameData ? <GameView gameData={gameData} /> : <Home setGameData={setGameData} />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App
--- src/components/GameView.tsx
+++ src/components/GameView.tsx
@@ -1,46 +1,52 @@
 import { useEffect, useState } from "react";
 import { GameData } from "../types";
 import React from "react";
+import { useNavigate } from 'react-router-dom';
 
 type GameViewProps = {
-  gameData: GameData
-}
+  gameData: GameData;
+};
 
-const GameView: React.FC<GameViewProps> = (props: GameViewProps) => {
-  const [round, setRound] = useState(1)
-  const [score, setScore] = useState(0)
+const GameView: React.FC<GameViewProps> = ({ gameData }) => {
+  const [round, setRound] = useState(1);
+  const [score, setScore] = useState(0);
+  const navigate = useNavigate();
 
-  useEffect(() => console.log(round, props.gameData), [round])
+  useEffect(() => console.log(round, gameData), [round]);
 
   const handlePickChoice = (id: number) => {
-    if (id === props.gameData.rounds[round - 1].correctCandidateId) {
-      setScore(score + 1)
+    if (id === gameData.rounds[round - 1].correctCandidateId) {
+      setScore(score + 1);
     }
-    setRound(round + 1)
-  }
+    setRound(round + 1);
+  };
 
   return (
     <div>
-      {round < 11 && 
+      {round < 11 ? (
         <div>
           {round} / 10
           <div>Pisteet: {score}</div>
+          <div>{gameData.rounds[round - 1].promise}</div>
           <div>
-            {props.gameData.rounds[round - 1].promise}
+            <button onClick={() => handlePickChoice(gameData.rounds[round - 1].candidates[0].id)}>
+              {gameData.rounds[round - 1].candidates[0].first_name} {gameData.rounds[round - 1].candidates[0].last_name}
+            </button>
           </div>
           <div>
-            <button onClick={() => handlePickChoice(props.gameData.rounds[round - 1].candidates[0].id)}>{props.gameData.rounds[round - 1].candidates[0].first_name} {props.gameData.rounds[round - 1].candidates[0].last_name}</button>
-          </div>
-          <div>
-            <button onClick={() => handlePickChoice(props.gameData.rounds[round - 1].candidates[1].id)}>{props.gameData.rounds[round - 1].candidates[1].first_name} {props.gameData.rounds[round - 1].candidates[1].last_name}</button>
+            <button onClick={() => handlePickChoice(gameData.rounds[round - 1].candidates[1].id)}>
+              {gameData.rounds[round - 1].candidates[1].first_name} {gameData.rounds[round - 1].candidates[1].last_name}
+            </button>
           </div>
         </div>
-      }
-      {round > 10 &&
-        <div>Game over! Score: {score}</div>
-      }
+      ) : (
+        <div>
+          <div>Peli ohi! Pisteet: {score}</div>
+          <button onClick={() => navigate('/')}>Takaisin päävalikkoon</button>
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default GameView
+export default GameView;
