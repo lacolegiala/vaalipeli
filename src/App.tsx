622fe092c80d361245e33a40d16257fbac7015b0
Sofia Riekkola <sofia.riekkola@gmail.com>
2025-04-03 16:36:02 +0300

    Try hash router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import './App.css';
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import GameView from './components/GameView';
 import { Candidate } from './types';
